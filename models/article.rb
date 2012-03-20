9bdd10e367fe2d6041b83c076451771a4bc46ea1
Dru <dru.reeve@gmail.com>
2012-03-20 11:33:44 +0000

    Re-add excerpt to Article

--- models/article.rb
+++ models/article.rb
@@ -1,20 +1,30 @@
 require_relative './post'
 
 class Article < Post
-  
+
   key :body,         String
   key :body_html,    String
-  
+  key :excerpt,      String
+  key :excerpt_html, String
+
   attr_accessible :body
-  
+
   validates_presence_of :body
-  
+
   before_save :format_content
-  
+
   protected
-  
+
   def format_content
     self.body_html = markdown.render(body).strip
+    # Set excerpt to nil if not specified or removed so that an empty string
+    # isn't saved in the database
+    if excerpt.blank?
+      self.excerpt = nil
+      self.excerpt_html = nil
+    else
+      self.excerpt_html = markdown.render(excerpt).strip
+    end
   end
-  
+
 end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,15 +1,27 @@
 require_relative '../spec_helper'
 
 describe Article do
-  
+
   it "should require a body" do
     article = Factory.build(:article, :body => '')
     article.should have_error_on(:body).with_message("can't be blank")
   end
-  
+
   it "should format body with markdown" do
     article = Factory.create(:article, :body => 'some _text_ using **markdown**')
     article.body_html.should eql("<p>some <em>text</em> using <strong>markdown</strong></p>")
   end
-  
+
+  it "should format excerpt with markdown" do
+    article = Factory.create(:article, :excerpt => 'some _text_ using **markdown**')
+    article.excerpt_html.should eql("<p>some <em>text</em> using <strong>markdown</strong></p>")
+  end
+
+  it "should set excerpt_html to nil when removing an excerpt" do
+    a = Factory.create(:article)
+    a.update_attributes(:excerpt => "")
+    a.reload.excerpt.should be_nil
+    a.reload.excerpt.should be_nil
+  end
+
 end
